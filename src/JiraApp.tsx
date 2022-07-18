100547c483012c0cbda9dc032529999fb7bc6244
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-18 17:57:16 -0500

    Added AppTheme

--- src/JiraApp.tsx
+++ src/JiraApp.tsx
@@ -2,16 +2,19 @@ import { useState } from "react"
 import { UserProvider } from "./context/User/UserProvider"
 import { AuthRouter, MainRouter } from "./router"
 import "./styles/globals.css"
+import { AppTheme } from "./styles/theme"
 
 
 const JiraApp = () => {
     const [isAuth, setisAuth] = useState(true)
     return (
-        <UserProvider>
-            {
-                isAuth === true ? <MainRouter /> : <AuthRouter />
-            }
-        </UserProvider>
+        <AppTheme>
+            <UserProvider>
+                {
+                    isAuth === true ? <MainRouter /> : <AuthRouter />
+                }
+            </UserProvider>
+        </AppTheme>
     )
 }
 
