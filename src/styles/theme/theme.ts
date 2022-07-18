9d8a746ee6af580036c3ac9208f4021fc709e43c
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-18 17:45:31 -0500

    Created Theme for the app

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            dark: "#1a237e",
+            main: "#f5f5f5",
+        }
+    }
+})
