b49d98f818d35e69e44d351e2688b72d44db834a
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-19 20:05:30 -0500

    Added background Color to Palette

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -2,9 +2,12 @@ import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
     palette: {
+        background: {
+            default: "#DDDDDD"
+        },
         primary: {
-            dark: "#1a237e",
-            main: "#f5f5f5",
+            dark: "#1A237E",
+            main: "#F5F5F5",
         }
     }
 })
