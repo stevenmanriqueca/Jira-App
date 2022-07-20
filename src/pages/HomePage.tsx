a3ada6320acfc38293af39428fd66fee6131855c
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-20 11:03:58 -0500

    Added OptionsHome Component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import { OptionsHome } from '../components/options';
 import { HomeContainer } from '../containers';
 
 const HomePage: FC = () => {
     return (
         <HomeContainer>
+            <OptionsHome />
             <div>HomePage</div>
         </HomeContainer>
     )
