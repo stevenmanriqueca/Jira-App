345e83cc0d6a161c6fca9897bb7e43d6dbafb74a
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-21 16:28:37 -0500

    Created StyledContainer and modify css properties of the other StyledComponents

--- src/styles/components/Options.tsx
+++ src/styles/components/Options.tsx
@@ -1,15 +1,40 @@
 import { Box, styled, FormLabel } from '@mui/material';
 
 
-
-export const StyledFormContainer = styled(Box)(() => ({
+export const StyledContainer = styled(Box)(({ theme }) => ({
     display: "flex",
-    justifyContent: "flex-end"
+    padding: 3,
+    [theme.breakpoints.up("xs")]: {
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    [theme.breakpoints.up("sm")]: {
+        flexDirection: "row"
+    },
+}))
+
+export const StyledFormContainer = styled(Box)(({ theme }) => ({
+    display: "flex",
+    [theme.breakpoints.up("xs")]: {
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+    },
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(2),
+        flexDirection: "row",
+    }
 }))
 
 export const StyledFormLabel = styled(FormLabel)(({ theme }) => ({
     color: theme.palette.primary.dark,
     paddingRight: theme.spacing(2),
-    paddingTop: theme.spacing(1),
-    fontSize: 17
+    fontSize: 17,
+    [theme.breakpoints.up("xs")]: {
+        paddingTop: theme.spacing(1),
+    },
+    [theme.breakpoints.up("sm")]: {
+        paddingTop: theme.spacing(0),
+    },
+
 }))
