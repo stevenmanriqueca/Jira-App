4dd072fde8a4a97993468888dd46cf8fde52dd92
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-21 16:23:26 -0500

    Added custom breakpoint in size sm and created custom scrollbars

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -9,5 +9,38 @@ export const theme = createTheme({
             main: "#1A237E",
             light: "#F5F5F5",
         },
-    }
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 744,
+            md: 900,
+            lg: 1200,
+            xl: 1536,
+        }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    // Styles for defult scrollbar Page
+                    "::-webkit-scrollbar": {
+                        width: "6px",
+                    },
+                    "::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#5e6188",
+                        borderRadius: "10px",
+                    },
+                    // Styles for All Scrollbars
+                    "*::-webkit-scrollbar": {
+                        width: "6px",
+                    },
+                    "*::-webkit-scrollbar-thumb": {
+                        backgroundColor: "#5e6188",
+                        borderRadius: "10px",
+                    },
+                },
+            },
+        },
+    },
 })
