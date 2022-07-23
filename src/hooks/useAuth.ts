7a3048e0f386aeb5db3225f0579d79efbeb6b539
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-23 16:33:46 -0500

    Created main function (checkAuthToken) for validate status

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,18 +1,26 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
+import { jiraApi } from '../api';
 import { UserContext } from '../context/User/UserContext';
+import { UserData } from '../interfaces/context-user/index';
 
 export const useAuth = () => {
 
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
-    const { loginUser, registerUser, state } = useContext(UserContext)
-    const { user: { id }, ui: { toggleLoading } } = state
+    const { loginUser, registerUser, onLogout, renewToken, state } = useContext(UserContext)
+    const { ui: { toggleLoading }, user: { status } } = state
 
-    useEffect(() => {
-        if (id) {
-            setIsAuthenticated(true)
+    const checkAuthToken = async () => {
+        const token = localStorage.getItem("token")
+        if (!token) return onLogout()
+        try {
+            const { data } = await jiraApi.get<UserData>("/user/renew");
+            localStorage.setItem("token", token)
+            renewToken(data)
         }
-    }, [id])
+        catch (err) {
+            localStorage.clear()
+            onLogout()
+        }
+    }
 
-
-    return { loginUser, registerUser, toggleLoading, isAuthenticated }
+    return { loginUser, registerUser, toggleLoading, checkAuthToken, status }
 }
