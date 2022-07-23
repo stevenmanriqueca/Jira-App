4aa16fb5778e5f978d4d785b757c8241720cceb2
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-22 19:18:24 -0500

    Changed reducers for errors by ToggleLoading

--- src/context/User/userReducer.ts
+++ src/context/User/userReducer.ts
@@ -3,51 +3,30 @@ import { State, UserData } from '../../interfaces/context-user/index';
 type userAction =
   | { type: 'userLogin', payload: UserData }
   | { type: "registerUser", payload: UserData }
-  | { type: 'userError', payload: { message: string } }
-  | { type: "cleanError" }
+  | { type: "toggleLoading" }
 
 export const userReducer = (state: State, action: userAction): State => {
   switch (action.type) {
     case 'userLogin':
       return {
         ...state,
-        user: {
-          ...action.payload,
-        },
+        user: { ...action.payload },
       };
 
     case "registerUser":
       return {
         ...state,
-        user: {
-          ...action.payload
-        }
+        user: { ...action.payload }
       };
 
-    case 'userError': {
-
+    case "toggleLoading": {
       return {
         ...state,
         ui: {
-          ...state.ui,
-          error: {
-            ...action.payload,
-          },
-        },
-      };
-    };
-
-    case "cleanError": {
-      return {
-        ...state,
-        ui: {
-          ...state.ui,
-          error: {
-            message: ""
-          },
+          toggleLoading: !state.ui.toggleLoading
         }
       }
-    };
+    }
 
     default:
       return state;
