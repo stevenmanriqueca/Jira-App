2b4ba36f4160933005821c5e18fce2cc31fc90af
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-22 19:16:00 -0500

    Modified Initial State and added functions of notifications(notistack)

--- src/context/User/UserProvider.tsx
+++ src/context/User/UserProvider.tsx
@@ -2,8 +2,9 @@ import { ReactNode, useReducer } from 'react';
 import { UserContext } from './UserContext';
 import { userReducer } from './userReducer';
 import { State } from '../../interfaces/context-user';
-import { Login, UserData, Register } from '../../interfaces/context-user/index';
+import { useSnackbar } from 'notistack';
 import jiraApi from '../../api/jiraApi';
+import { Login, UserData, Register } from '../../interfaces/context-user/index';
 
 const initialState: State = {
   user: {
@@ -13,10 +14,7 @@ const initialState: State = {
     token: '',
   },
   ui: {
-    error: {
-      message: '',
-    },
-    loading: false,
+    toggleLoading: false,
   },
 };
 
@@ -26,24 +24,32 @@ interface Props {
 
 export const UserProvider = ({ children }: Props) => {
   const [state, dispatch] = useReducer(userReducer, initialState);
+  const { enqueueSnackbar } = useSnackbar()
 
   const loginUser = async ({ email, password }: Login) => {
     try {
-      dispatch({ type: "cleanError" })
+      dispatch({ type: "toggleLoading" })
       const { data } = await jiraApi.post<UserData>('/user', { email, password });
       dispatch({ type: 'userLogin', payload: data });
+      enqueueSnackbar(`Welcome ${data.name}.`, { variant: "success" })
     } catch (error) {
-      dispatch({ type: 'userError', payload: { message: 'Email or password incorrect!' } });
+      enqueueSnackbar("Email or Password incorrect.", { variant: "error" })
+    } finally {
+      dispatch({ type: "toggleLoading" })
     }
   };
 
   const registerUser = async ({ email, name, password }: Register) => {
     try {
-      dispatch({ type: "cleanError" })
+      dispatch({ type: "toggleLoading" })
       const { data } = await jiraApi.post<UserData>('/user/register', { email, name, password });
       dispatch({ type: 'registerUser', payload: data });
+      enqueueSnackbar(`Welcome ${data.name}.`, { variant: "success" })
     } catch (error) {
-      dispatch({ type: 'userError', payload: { message: 'An error has ocurred, Please try again.' } });
+      dispatch({ type: "toggleLoading" })
+      enqueueSnackbar("An error has ocurred, Please try again.", { variant: "error" })
+    } finally {
+      dispatch({ type: "toggleLoading" })
     }
   }
 
