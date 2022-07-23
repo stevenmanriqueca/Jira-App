5061462f7edb5535122f5913becdece161f0cd42
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-22 19:03:45 -0500

    Added SnackbarProvider from notistack

--- src/JiraApp.tsx
+++ src/JiraApp.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react"
 import { UserProvider } from "./context/User/UserProvider"
+import { AppTheme } from "./styles/theme"
+import { SnackbarProvider } from "notistack"
 import { AuthRouter, MainRouter } from "./router"
 import "./styles/globals.css"
-import { AppTheme } from "./styles/theme"
 
 
 const JiraApp = () => {
-    const [isAuth, setisAuth] = useState(true)
+    const [isAuth, setisAuth] = useState(false)
     return (
         <AppTheme>
-            <UserProvider>
-                {
-                    isAuth === true ? <MainRouter /> : <AuthRouter />
-                }
-            </UserProvider>
+            <SnackbarProvider maxSnack={3}>
+                <UserProvider>
+                    {
+                        isAuth === true ? <MainRouter /> : <AuthRouter />
+                    }
+                </UserProvider>
+            </SnackbarProvider>
         </AppTheme>
     )
 }
