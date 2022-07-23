b2bd893505a3f32572b2d37e314bfb3926023861
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-23 16:27:40 -0500

    Added typed of functions for ContextProps

--- src/context/User/UserContext.ts
+++ src/context/User/UserContext.ts
@@ -1,11 +1,13 @@
 import { createContext } from 'react';
 import { State, Login } from '../../interfaces/context-user';
-import { Register } from '../../interfaces/context-user/index';
+import { Register, UserData } from '../../interfaces/context-user/index';
 
 export type ContextProps = {
   state: State;
   loginUser: (data: Login) => void;
   registerUser: (data: Register) => void;
+  renewToken: (data: UserData) => void;
+  onLogout: () => void;
 };
 
 export const UserContext = createContext<ContextProps>({} as ContextProps);
