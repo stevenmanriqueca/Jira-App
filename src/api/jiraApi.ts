e5cdf44a500fb1c440b1599666a5b66e0e3614f2
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-23 16:22:50 -0500

    Added axios interceptor for token

--- src/api/jiraApi.ts
+++ src/api/jiraApi.ts
@@ -4,4 +4,12 @@ const jiraApi = axios.create({
   baseURL: 'https://jira-server-st.herokuapp.com/jira',
 });
 
+jiraApi.interceptors.request.use(config => {
+  config.headers = {
+    ...config.headers,
+    "x-token": localStorage.getItem("token") || ""
+  }
+  return config
+})
+
 export default jiraApi;
