266a753113c96bc4dc162cd77831a8916ff0dc76
stevenmanriqueca <stevenmnrq@gmail.com>
2022-08-03 19:53:37 -0500

    Added Theme, notisnack and User provider

--- src/JiraApp.tsx
+++ src/JiraApp.tsx
@@ -1,6 +1,18 @@
+import { UserProvider } from "./context/User/UserProvider"
+import { AppTheme } from "./styles/theme"
+import { SnackbarProvider } from "notistack"
+import { MainContainer } from "./containers"
+import "./styles/globals.css"
+
 const JiraApp = () => {
     return (
-        <div>JiraApp</div>
+        <AppTheme>
+            <SnackbarProvider maxSnack={3}>
+                <UserProvider>
+                    <MainContainer />
+                </UserProvider>
+            </SnackbarProvider>
+        </AppTheme>
     )
 }
 
