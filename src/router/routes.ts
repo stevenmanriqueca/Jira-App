25dc34c2c0c1641677c3735bc595c45c498ca82d
stevenmanriqueca <stevenmnrq@gmail.com>
2022-08-03 19:12:45 -0500

    Created Routing for the app

---
+++ src/router/routes.ts
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { HomePage, AuthPage, NotesPage } from '../pages';
+
+type JSXComponent = FC
+
+interface RouteMain {
+    path: string;
+    Component: JSXComponent;
+    to: string;
+    name: string;
+}
+
+interface RouteAuth extends Omit<RouteMain, "to" | "name"> { };
+
+
+export const mainRoutes: RouteMain[] =
+    [
+        {
+            path: "/",
+            Component: HomePage,
+            to: "/",
+            name: "Jira"
+        },
+    ]
+
+
+export const authRoutes: RouteAuth[] =
+    [
+        {
+            path: "/auth",
+            Component: AuthPage,
+        }
+    ]
