c10d691420dd084722b5cc14bf23a9f734e8b556
stevenmanriqueca <stevenmnrq@gmail.com>
2022-08-11 11:51:17 -0500

    Implemented Styles for Entry Card

--- src/components/board/EntryCard.tsx
+++ src/components/board/EntryCard.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
-import { Paper, Stack, Typography, Box, IconButton, Chip } from "@mui/material"
+import { Stack, Typography, Box, IconButton, Chip } from "@mui/material"
+import { StyledCardContainer, StyledCardTitle, StyledCardDescription, StyledCardTagsContainer } from "../../styles/components";
 import { Draggable } from "react-beautiful-dnd"
-import { Entry } from "../../interfaces/context-entries";
+import { useModal } from "../../hooks/useModal";
+import { useOptions } from "../../hooks/useOptions";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
-import { useOptions } from '../../hooks/useOptions';
+import { Entry } from "../../interfaces/context/entries";
 
 interface Props extends Entry {
     index: number;
@@ -12,6 +14,7 @@ interface Props extends Entry {
 
 export const EntryCard: FC<Props> = ({ index, _id, title, description, priority, userTags }) => {
     const { deleteEntry } = useOptions()
+    const { openModalEditEntry } = useModal()
     return (
         <Draggable
             draggableId={_id}
@@ -19,58 +22,42 @@ export const EntryCard: FC<Props> = ({ index, _id, title, description, priority,
         >
             {(provided) => {
                 return (
-                    <Paper
+                    <StyledCardContainer elevation={6} sx={{ ...provided.draggableProps.style }}
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        elevation={6}
-                        sx={{
-                            borderRadius: "20px",
-                            margin: "7px",
-                            maxHeight: "300px",
-                            cursor: "grab",
-                            ...provided.draggableProps.style,
-                        }}>
+                    >
                         <Stack direction="row" justifyContent="space-between" >
-                            <Typography variant="h6" align='left' sx={{ fontWeight: "book", m: 2, color: "primary.main" }}>
+                            <StyledCardTitle variant="h6" align='left'>
                                 {title}
-                            </Typography>
+                            </StyledCardTitle>
                             <Box>
-                                <IconButton size="small" sx={{ m: 1 }} >
+                                <IconButton size="small" sx={{ m: 1 }} onClick={() => openModalEditEntry(_id)}>
                                     <EditIcon sx={{ color: "primary.main" }} />
                                 </IconButton>
-                                <IconButton size="small" sx={{ mr: 1 }} onClick={() => deleteEntry(_id)}>
+                                <IconButton size="small" sx={{ mr: 1 }} onClick={() => deleteEntry(_id)} >
                                     <DeleteIcon sx={{ color: "#b61818" }} />
                                 </IconButton>
                             </Box>
                         </Stack>
-                        <Box sx={{
-                            width: "100%", height: "100px", px: 1.2,
-                        }}>
-                            <Typography variant="body1" sx={{
-                                display: "-webkit-box",
-                                height: "100px",
-                                WebkitLineClamp: "4",
-                                WebkitBoxOrient: "vertical",
-                                overflow: "hidden",
-                            }}>
+                        <Box sx={{ width: "100%", height: "100px", px: 1.2 }}>
+                            <StyledCardDescription variant="body1">
                                 {description}
-                            </Typography>
+                            </StyledCardDescription>
                         </Box>
                         <Stack direction="row" mx={3} my={1}>
                             <Typography sx={{ m: 0.6, fontWeight: "bolder" }} variant="body1">Priority:</Typography>
-                            <Chip label={priority} variant="outlined"
-                                color={priority === "High" ? "error" : priority === "medium" ? "warning" : "success"} size="medium"
+                            <Chip label={priority} color={priority === "High" ? "error" : priority === "Medium" ? "warning" : "success"} size="medium"
                             />
                         </Stack>
-                        <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center", my: 0.6 }} >
+                        <StyledCardTagsContainer>
                             {
                                 userTags.map(({ _id, title }) => (
-                                    <Chip sx={{ m: 0.8 }} key={_id} label={title} variant="outlined" color="primary" size="medium" />
+                                    <Chip sx={{ m: 0.8 }} key={_id} label={title} color="primary" size="medium" variant="outlined" />
                                 ))
                             }
-                        </Box>
-                    </Paper>
+                        </StyledCardTagsContainer>
+                    </StyledCardContainer>
                 )
             }}
         </Draggable >
