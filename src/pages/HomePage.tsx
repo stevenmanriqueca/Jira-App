a2df0dcf2f2735af04c53d374832aab4d95a806f
stevenmanriqueca <stevenmnrq@gmail.com>
2022-08-11 11:57:04 -0500

    Implemented ConxtexDnD and feedback spinner when loading app

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,41 @@
 import { FC } from 'react';
+import { CircularProgress, Grid, Stack } from '@mui/material';
 import { HomeContainer } from '../containers';
+import { useJiraBoard } from '../hooks/useJiraBoard';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { OptionsHome } from '../components/options';
+import { Column } from '../components/board';
+import { Modal } from '../components/modal';
+
 
 const HomePage: FC = () => {
+
+    const { columns, loading, onDragEnd } = useJiraBoard()
+
+    if (loading) {
+        return (
+            <Stack alignItems="center" sx={{ mt: "35vh" }} >
+                <CircularProgress color="primary" />
+            </Stack>
+        )
+    }
+
     return (
         <HomeContainer>
-            HOLA
+            <Modal />
+            <OptionsHome />
+            <Grid container spacing={1} justifyContent="center" >
+                <DragDropContext
+                    onDragEnd={result => onDragEnd(result)}
+                >
+                    {Object.entries(columns).map(([columnId, column]) => {
+                        const { name, items } = column
+                        return (
+                            <Column key={columnId} columnId={columnId} nameColumn={name} itemsColumn={items} />
+                        );
+                    })}
+                </DragDropContext>
+            </Grid>
         </HomeContainer >
     )
 }
